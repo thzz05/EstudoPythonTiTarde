3357e1a6d84d6326789f3e67d0aa98dd3e49f567
thiago porfirio malagolin <thzz.malagolin@gmail.com>
2024-02-29 15:29:14 -0300

    init: iniciando novo estudo em Python. Exibindo mensagens

---
+++ aulas01/boasvindas.py
@@ -0,0 +1,7 @@
+# Vamos criar uma variavel para armazenar o 
+# nome do usuario
+nome = "admin"
+print("Olá, "+nome+". Seja bem vindo")
+print("escreva o seu novo nome de usuario:")
+nome = input("digite aqui:")
+print("o seu novo usuario é : " +nome)

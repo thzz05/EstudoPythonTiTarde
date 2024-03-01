86ea6d567324fd7020335b1cf605e4f4dabdc2ae
thiago porfirio malagolin <thzz.malagolin@gmail.com>
2024-03-01 14:18:45 -0300

    Aula02-primeiro arquivo match

---
+++ Aula02/match.py
@@ -0,0 +1,21 @@
+print("este progama analisa os valores")
+    digitado de 0 a 6 e diz o dia da semana")
+    digito = input("entre com um numero de 0 a 6")
+    
+    match digito:
+        case '0':
+            print("Domingo")
+        case '1':
+            print("Segunda-SemFeira")    
+        case '2':
+            print("Terça-feira")
+        case '3':
+            print("Quarta-SemFeira")
+        case '4':
+            print("Quinta-feira")    
+        case '5':   
+            print("Sexta-SemFeira")
+        case '6':
+            print("Sabado-feira")           
+
+                                       
